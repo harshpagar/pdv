7f66ac526b619ea8348525dc9ad58afd8f77e430
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-04 17:21:43 +0530

    add

--- pdv/src/Components/Home.jsx
+++ pdv/src/Components/Home.jsx
@@ -9,7 +9,22 @@ export const  Home = () =>
     return (
         <>
         <Navbar/>
-        <span className="name">Prospect Set</span>
+        
+
+        <div className="table">
+            <div className="name">
+                <span >Prospect Set</span><br/>
+            </div>
+           <div className="top">
+                <span>Customer</span>
+                <span>Prospect Customer</span>
+                <span>Employee</span>
+                <span>Test Set</span>
+           </div>
+           
+        </div>
+
+
         </>
     )
 }
