f0dc6034f83444cc8b294b1ff207b75996e7ab1f
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-04 17:05:33 +0530

    navbar

---
+++ pdv/src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../Styles/Home.css';
+import { Navbar } from "./Navbar";
+
+
+export const  Home = () => 
+{
+
+    return (
+        <>
+        <Navbar/>
+        <span className="name">Prospect Set</span>
+        </>
+    )
+}
---
+++ pdv/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import '../Styles/Navbar.css';
+
+export const  Navbar = () => 
+{
+
+    return (    
+        <>
+            <div className="main">
+                    <h1>hii</h1>
+                <div className="but">
+                    <button className="one">Trigger Event </button>
+                    <button className="two">Email template </button>
+                    <button className="three">Prospect Set </button>
+                    <button className="four">Campaign </button>
+
+                </div>
+            </div>
+        </>
+    )
+}
