2ea0653e41abe53d0d1013a10c3f4d124e80d84c
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-21 14:56:14 +0530

    add

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,12 @@
-const mongoose=require('mongoose')
-const connect=()=>{
-    return mongoose.connect('mongodb+srv://pdv:pdv@cluster0.ned56.mongodb.net/assignmentdb',{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-        });
-}
+const mongoose = require("mongoose");
+const connect = () => {
+  return mongoose.connect(
+    "mongodb+srv://pdv:pdv@cluster0.ned56.mongodb.net/assignmentdb",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+};
 
-module.exports=connect;
+module.exports = connect;
